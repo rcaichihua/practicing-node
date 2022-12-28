b3595503c18b1dbf7c59cab91fa432dbee8df7a6
Roger Davy <rcaichihua@gmail.com>
2022-12-28 18:14:49 -0500

    Inicio segunda carga

---
+++ clase02/app.js
@@ -0,0 +1,10 @@
+let base = 5;
+
+console.clear();
+console.log('============================');
+console.log(`=====tabla del ${base}======`);
+console.log('============================');
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${base} x ${i} = ${base * i}`)
+}
